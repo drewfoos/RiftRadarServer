3d458944f90357e2d6c0cd011011b4265b05ad41
drewfoos <drewfoos@yahoo.com>
2023-08-28 22:14:36 -0400

    server

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/drewfoos/RiftRadarServer/vendor/handlers"
+	"github.com/drewfoos/RiftRadarServer/vendor/middleware"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	handlers.InitApiKey()
+
+	// Create a new Fiber instance
+	app := fiber.New()
+
+	// Add middleware
+	app.Use(middleware.Cors())
+
+	// Define routes
+	app.Post("/search", handlers.SearchHandler)
+
+	// Health check
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.SendString("Server is healthy")
+	})
+
+	// Start the server
+	err := app.Listen(":8080")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
